dcf15dd86b39169c4ef6ac3880074a9c0de609d0
3mroos <86962039+3mroos@users.noreply.github.com>
2025-07-09 13:56:16 +0300

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      screens: {
+        DEFAULT: '1440px',
+        sm: '640px',
+        md: '768px',
+        lg: '1440px',
+        xl: '1440px',
+      }
+    },
+    extend: {
+      colors: {
+        gray: {
+          500: '#717680',
+          600: '#535862',
+          700: '#414651',
+          800: '#252B37',
+        },
+        blue: {
+          700: '#175CD3'
+        }
+      },
+      maxWidth: {
+        '1440': '1440px',
+      }
+    },
   },
   plugins: [],
 };

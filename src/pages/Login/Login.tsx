bb24054004d5bfa17949ee36fe88dd307ae8dd50
3mroos <86962039+3mroos@users.noreply.github.com>
2025-07-09 14:41:42 +0300

    login layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -48,7 +48,7 @@ const LoginForm = () => {
     <>
       <div className="flex max-w-1440 mx-auto min-h-screen">
 
-        <div className=" flex-1 flex flex-col  justify-center items-center">
+        <div className=" flex-1 flex flex-col  justify-center items-center relative">
           <div className="max-w-[360px]">
             <div className="mb-10">
               <img src="/src/assets/svg/logo_with_text.svg" alt="Logo" />
@@ -57,6 +57,50 @@ const LoginForm = () => {
               <div className="font-bold text-4xl mb-3">Login</div>
               <div className="text-gray-600">Welcome back! Please enter your details.</div>
             </div>
+
+            <div>
+              <div className="mb-5">
+                <label className="">
+                  <div className="mb-1.5 text-sm text-gray-600 font-semibold">Email</div>
+                  <input
+                    type="text"
+                    placeholder="Enter your email"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-lg mb-4"
+                    {...useForm<FormLoginValues>().register("name", { required: true })}
+                  />
+                </label>
+              </div>
+              <div className="mb-6">
+                <label className="">
+                  <div className="mb-1.5 text-sm text-gray-600 font-semibold">Password</div>
+                  <input
+                    type="password"
+                    placeholder="Enter your email"
+                    className="w-full px-4 py-2 border border-gray-300 rounded-lg mb-4"
+                    {...useForm<FormLoginValues>().register("name", { required: true })}
+                  />
+                </label>
+              </div>
+
+              <div className="flex justify-between mb-6">
+                <label className="flex items-center gap-2">
+                  <input type="checkbox" className="w-4 h-4 border-gray-300" />
+                  Remember me
+                </label>
+                <a href="#" className="text-blue-700 font-bold hover:underline">Forgot Password?</a>
+              </div>
+
+              <button className="text-white font-bold bg-blue-700 p-2.5 w-full rounded-lg">Sign in</button>
+            </div>
+
+            <div className="mt-9 text-sm text-gray-600 text-center">
+              Don’t have an account?
+              <a href="#" className="text-blue-700 font-bold hover:underline mx-2">Sign up</a>
+            </div>
+          </div>
+
+          <div className="font-medium text-sm text-gray-600 absolute bottom-8 left-1/2 transform -translate-x-1/2">
+            © ALPHAS 2025
           </div>
         </div>
 
